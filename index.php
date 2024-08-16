e66ff660fe5c1208bf1d5d4bf008d01b335cf2c1
Maksim <max128kaz08@gmail.com>
2024-08-17 00:13:31 +0300

    index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        Teach PHP
+    </h1>
+    <?php
+        echo '1';
+        print_r('2');
+        echo '<br>';
+        $x = 'php';
+        echo 'I love'.$x.'!';
+
+
+
+    ?>
+</body>
+</html>
